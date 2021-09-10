373eca5ba2a81d844e82383b965045d8627216c7
nishabaruwal576 <56593929+nishabaruwal576@users.noreply.github.com>
2021-09-10 23:24:57 +0545

    Add files via upload

---
+++ Book/template/footer.php
@@ -0,0 +1,8 @@
+	<html>
+  <body>
+  <div style="background-color:black; text-align:center; opacity:33%;">
+   <!-- /container -->
+   Copyright &copy; 2021 Nisha Baruwal
+   </div>
+  </body>
+</html>
